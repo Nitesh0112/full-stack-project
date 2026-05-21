2a7265916c6c0377ed56c3524fccdb70933252b0
Nitesh <rnitesh0908@gmail.com>
2026-05-21 15:08:47 +0530

    rename middleware file

--- routes/listing.js
+++ routes/listing.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const Listing = require("../models/listing.js");
 const wrapAsync = require("../utils/wrapAsync.js");
-const {isLoggedIn, isOwner, validateListing} = require("../middleware.js");
+const {isLoggedIn, isOwner, validateListing} = require("../middlewares.js");
 const { populate } = require("../models/review.js");
 
 const listingController = require("../controllers/listings.js");
